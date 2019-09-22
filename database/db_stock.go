437e41b42e988bf1889b6be36eb4f901a3492480
Huy <tqhuy1996.developer@gmail.com>
2019-09-22 15:39:27 +0700

    fix source

--- database/db_stock.go
+++ database/db_stock.go
@@ -2,8 +2,6 @@ package database
 import (
 	"context"
 	"errors"
-	"time"
-	"strconv"
 	"github.com/tranhuy-dev/IStockGolang/models"
 	"github.com/tranhuy-dev/IStockGolang/core/constant"
 )
@@ -30,10 +28,4 @@ func CreateStock(stock models.Stock) (interface{} , error){
 		return nil,errors.New("Insert fail") 
 	}
 	return insertResult, nil
-}
-
-func getIDStock() (string) {
-	t := time.Now()
-	timestamps := strconv.Itoa(int(t.Unix()))
-	return timestamps
 }
